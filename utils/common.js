57dc30eb217b7e836bc784b9d60f6dc24b03056e
wangfy263 <wfy339095626@gmail.com>
2019-07-07 14:39:39 +0800

    fix weekrange

--- utils/common.js
+++ utils/common.js
@@ -27,8 +27,8 @@ common.getWeekRange = () => {
   let start = moment().startOf('week').add(1, 'd').format('MMDD');
   let end = moment().endOf('week').subtract(1, 'd').format('MMDD');
   if(moment().format('MMDD') === '0707'){
-    start === '0701';
-    end === '0705';
+    start = '0701';
+    end = '0705';
   }
   return start + "-" + end;
 }
