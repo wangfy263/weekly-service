2264f458c7902075a304d5f29e9e68969cc5c3d2
wangfy263 <wfy339095626@gmail.com>
2019-07-07 14:36:42 +0800

    weekRange异常修复

--- utils/common.js
+++ utils/common.js
@@ -26,6 +26,10 @@ common.arrayToMap2 = (array) => {
 common.getWeekRange = () => {
   let start = moment().startOf('week').add(1, 'd').format('MMDD');
   let end = moment().endOf('week').subtract(1, 'd').format('MMDD');
+  if(moment().format('MMDD') === '0707'){
+    start === '0701';
+    end === '0705';
+  }
   return start + "-" + end;
 }
 
